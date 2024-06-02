0ef2b40b63a980eb08cf77b06f6cc3ab7edaf1cc
Serhii5465 <serhiiartiukh5@gmail.com>
2024-06-02 11:53:32 +0300

    CheckStatusSSH.groovy - update

--- vars/CheckStatusSSH.groovy
+++ vars/CheckStatusSSH.groovy
@@ -1,9 +1,13 @@
 def call(String id){
     sshPublisher failOnError: true, 
     publishers: [sshPublisherDesc(configName: "${id}", sshRetry: [retries: 3, retryDelay: 1000], 
-    transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'whoami; uname -r', execTimeout: 120000)], verbose: true)]
+    transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'hostname; whoami; uname -r', execTimeout: 120000)], verbose: true)]
 
     if(currentBuild.result == 'SUCCESS'){
         echo "${id} online"
+    }
+
+    if(currentBuild.result == 'FAILURE'){
+        error("${id} offline")
     }
 }
